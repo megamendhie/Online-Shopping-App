4461ce9170791bc2efee0df988129559e31f5171
megamendhie <imendhie@yahoo.com>
2021-05-03 13:33:55 +0100

    Order details lookup

---
+++ app/src/main/java/com/sqube/desantosdirectory/ProductRequestDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.sqube.desantosdirectory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductRequestDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_request_details);
+    }
+}
---
+++ app/src/main/java/com/sqube/desantosdirectory/ServiceRequestDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.sqube.desantosdirectory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceRequestDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_request_details);
+    }
+}
