2e59dee03a92f781919c29105f6d2006f4408d31
megamendhie <imendhie@yahoo.com>
2021-05-06 16:21:38 +0100

    Product class implements Parcelable

--- app/src/main/java/models/Product.java
+++ app/src/main/java/models/Product.java
@@ -1,6 +1,11 @@
 package models;
 
-public class Product {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
+public class Product implements Parcelable {
     private String name;
     private String icon;
     private String docId;
@@ -11,6 +16,44 @@ public class Product {
     private long createdAt;
     private long clicks;
     private int quantity;
+
+    public Product(){}
+
+    public Product(String name, String icon, String docId, String categoryId, String size, boolean visible, long price){
+        this.name = name;
+        this.icon = icon;
+        this.docId = docId;
+        this.categoryId = categoryId;
+        this.size = size;
+        this.visible = visible;
+        this.price = price;
+        this.createdAt = new Date().getTime();
+    }
+
+    protected Product(Parcel in) {
+        name = in.readString();
+        icon = in.readString();
+        docId = in.readString();
+        categoryId = in.readString();
+        size = in.readString();
+        visible = in.readByte() != 0;
+        price = in.readLong();
+        createdAt = in.readLong();
+        clicks = in.readLong();
+        quantity = in.readInt();
+    }
+
+    public static final Creator<Product> CREATOR = new Creator<Product>() {
+        @Override
+        public Product createFromParcel(Parcel in) {
+            return new Product(in);
+        }
+
+        @Override
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -90,5 +133,24 @@ public class Product {
 
     public void setVisible(boolean visible) {
         this.visible = visible;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(icon);
+        dest.writeString(docId);
+        dest.writeString(categoryId);
+        dest.writeString(size);
+        dest.writeByte((byte) (visible ? 1 : 0));
+        dest.writeLong(price);
+        dest.writeLong(createdAt);
+        dest.writeLong(clicks);
+        dest.writeInt(quantity);
     }
 }
