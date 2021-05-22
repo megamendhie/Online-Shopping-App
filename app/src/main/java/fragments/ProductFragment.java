35f76998b09fd056ea9f8bcc7b80ed576c10f588
megamendhie <imendhie@yahoo.com>
2021-05-22 09:19:09 +0100

    added profile link to product fragment

--- app/src/main/java/fragments/ProductFragment.java
+++ app/src/main/java/fragments/ProductFragment.java
@@ -2,6 +2,7 @@ package fragments;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
@@ -9,18 +10,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.firestore.Query;
+import com.google.gson.Gson;
 import com.sqube.desantosdirectory.LoginActivity;
 import com.sqube.desantosdirectory.ProfileActivity;
 import com.sqube.desantosdirectory.R;
 import com.sqube.desantosdirectory.SearchActivity;
 
 import adapters.CategoryAdapter;
+import de.hdodenhof.circleimageview.CircleImageView;
+import models.User;
 import utils.FirebaseUtil;
 
 import static models.Commons.CATEGORIES;
@@ -38,6 +44,8 @@ import static models.Commons.VISIBLE;
 public class ProductFragment extends Fragment {
     private RecyclerView lstProducts;
     private CategoryAdapter adapter;
+    private CircleImageView imgDp;
+    private final Gson gson = new Gson();
 
     public ProductFragment() {
         // Required empty public constructor
@@ -52,12 +60,9 @@ public class ProductFragment extends Fragment {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2);
         lstProducts.setLayoutManager(gridLayoutManager);
         ImageView imgHome = view.findViewById(R.id.imgHome);
-        imgHome.setOnClickListener(v -> {
-            if(FirebaseUtil.getAuth().getCurrentUser()==null)
-                startActivity(new Intent(getContext(), LoginActivity.class));
-            else
-                startActivity(new Intent(getContext(), ProfileActivity.class));
-        });
+        imgHome.setOnClickListener(v -> openProfile());
+        imgDp = view.findViewById(R.id.imgDp);
+        imgDp.setOnClickListener(v -> openProfile());
         CardView crdSearch = view.findViewById(R.id.search_cardView);
         crdSearch.setOnClickListener(view1 -> {
             Intent intent = new Intent(getContext(), SearchActivity.class);
@@ -66,6 +71,13 @@ public class ProductFragment extends Fragment {
         });
         loadProductCategories();
         return view;
+    }
+
+    private void openProfile(){
+        if(FirebaseUtil.getAuth().getCurrentUser()==null)
+            startActivity(new Intent(getContext(), LoginActivity.class));
+        else
+            startActivity(new Intent(getContext(), ProfileActivity.class));
     }
 
     private void loadProductCategories() {
@@ -91,4 +103,23 @@ public class ProductFragment extends Fragment {
             adapter.startListening();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(FirebaseUtil.getAuth().getCurrentUser()==null){
+            imgDp.setImageResource(R.drawable.ic_account_circle_green_64);
+        }
+        else{
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext().getApplicationContext());
+            String json = prefs.getString("profile", "");
+            User myProfile = (json.equals("")) ? null : gson.fromJson(json, User.class);
+            if(myProfile ==null)
+                return;
+
+            if(myProfile.getPic()==null||myProfile.getPic().isEmpty())
+                imgDp.setImageResource(R.drawable.ic_account_circle_green_64);
+            else
+                Glide.with(getContext()).load(myProfile.getPic()).into(imgDp);
+        }
+    }
 }
